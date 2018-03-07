a6ada3b8fc97da1f91fd598ebb390f3356395bf4
Madison Barnett <Madison.barnett7@gmail.com>
2018-03-07 12:27:41 -0500

    Add files via upload

---
+++ config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: '7f78e10786184c9db225',
+  GITHUB_CLIENT_SECRET: '43ca396f9b75f9ff206985e33fc0c6636755273a',
+  GITHUB_CALLBACK_URL: 'https://localhost:8080/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017/reforum',
+};
